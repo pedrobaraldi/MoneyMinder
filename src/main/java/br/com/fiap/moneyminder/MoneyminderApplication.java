732aff104bd6a61b13f277e3de9092e220c80b45
pedrobaraldi <pedrobaraldi02@gmail.com>
2024-03-18 09:34:46 -0300

    first commit

---
+++ src/main/java/br/com/fiap/moneyminder/MoneyminderApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.moneyminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyminderApplication.class, args);
+	}
+
+}
